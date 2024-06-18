73b80077f2633052856b46024dc748c48a7e1923
Carla-Sofia-Centeleghe <81233915+Carla-Sofia-Centeleghe@users.noreply.github.com>
2024-06-18 00:40:33 -0300

    Modifico TP 1, 2 , 3 Y 4

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -3,25 +3,32 @@ import matplotlib.pyplot as plt
 
 # Datos del calentador de agua
 masa_agua_kg = 1  # Masa de agua en kg
-capacidad_termica_agua_J_kgC = 4.186  # Capacidad térmica del agua en J/kg°C
-potencia_resistencia_W = 108  # Potencia de la resistencia eléctrica en W
-tiempo_simulacion_s = 8  # Tiempo de simulación en segundos
+capacidad_termica_agua_J_kgC = 4186  # Capacidad térmica del agua en J/kg°C 
+voltaje_V = 110  # Voltaje suministrado en V
+resistencia_Ohm = 108  # Resistencia eléctrica en Ohm
 temperatura_inicial_C = 10  # Temperatura inicial del agua en °C
 temperatura_objetivo_C = 75  # Temperatura objetivo del agua en °C
 delta_t = 1  # Paso del tiempo en segundos
+
+# Calculo de la corriente usando la Ley de Ohm
+corriente_A = voltaje_V / resistencia_Ohm
 
 # Inicializo listas para guardar datos de la simulación
 tiempo = [0]  # Lista para el tiempo
 temperatura = [temperatura_inicial_C]  # Lista para la temperatura
 
 # Realizo la simulación
-for t in range(1, tiempo_simulacion_s + 1):
+for t in range(1, int(2520/delta_t) + 1):  # Ajusto el rango de tiempo para llegar a 42 minutos
+    # Calculo la potencia usando la corriente y la ley de Ohm
+    potencia_W = corriente_A**2 * resistencia_Ohm
+    
     # Calculo cambio de temperatura usando la potencia de la resistencia
-    delta_temperatura = (potencia_resistencia_W * delta_t) / (masa_agua_kg * capacidad_termica_agua_J_kgC)
+    delta_temperatura = potencia_W * delta_t / (masa_agua_kg * capacidad_termica_agua_J_kgC)
+    
     nueva_temperatura = temperatura[-1] + delta_temperatura
 
     # Actualizo listas del tiempo y de la temperatura
-    tiempo.append(t)
+    tiempo.append(t*delta_t)
     temperatura.append(nueva_temperatura)
 
     # Se detiene la simulación si alcanzo la temperatura objetivo
@@ -34,7 +41,7 @@ plt.plot(tiempo, temperatura, label='Temperatura del agua')
 plt.axhline(y=temperatura_objetivo_C, color='r', linestyle='--', label='Temperatura objetivo')
 plt.xlabel('Tiempo (segundos)')
 plt.ylabel('Temperatura (°C)')
-plt.title('Simulación de calentador de agua')
+plt.title('Simulación de calentador de agua usando Ley de Ohm')
 plt.legend()
 plt.grid(True)
 plt.show()
--- TP4/TP4_interfaz.py
+++ TP4/TP4_interfaz.py
@@ -1,8 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import messagebox
+
 
 class CalentadorH2O:
     def __init__(self):
+        
+        self.root = tk.Tk()
+        self.root.title("Graficador de Pérdida de Calor")
+        self.create_widgets()
+
         self.radio = 0.05  # Radio del calentador en metros
         self.altura = 0.14  # Altura del calentador en metros
         self.espesores = [0.01, 0.005, 0.05]  # Definición de los espesores de las paredes del calentador en metros
@@ -10,12 +19,13 @@ class CalentadorH2O:
         self.temperatura_objetivo_C = 75  # Temperatura objetivo del agua en °C
         self.temperatura_externa_C = 25  # Temperatura externa en °C
         self.delta_t = 1  # Paso del tiempo en segundos
-        self.tiempo_max = 3500  # Tiempo máximo de simulación en segundos
+        self.tiempo_max = 2500  # Tiempo máximo de simulación en segundos
         self.voltaje_V = 110  # Voltaje suministrado en V
         self.resistencia_Ohm = 108  # Resistencia eléctrica en Ohm
         self.qesph2o = 4186  # Capacidad térmica del agua en J/kg°C
         self.k = 1  # Conductividad térmica del material en W/m.K
         self.area = 2 * np.pi * self.radio * self.altura + 2 * np.pi * self.radio**2  # Área total del cilindro
+        self.qent = self.calcular_potencia()  # Cálculo de la potencia utilizando la Ley de Ohm
 
     def calcular_potencia(self):
         # Cálculo de la potencia entregada al sistema por unidad de tiempo
@@ -81,16 +91,16 @@ class CalentadorH2O:
         plt.grid(True)
         plt.show()
 
+    def create_widgets(self):
+            self.sin_perdidas_button = tk.Button(self.root, text="Sin Pérdida de Calor",bg="blue", fg="white", command=self.sin_perdidas)
+            self.sin_perdidas_button.pack(side="top")
+
+            self.con_perdidas_button = tk.Button(self.root, text="Con Pérdida de Calor",bg="green", fg="white", command=self.con_perdidas)
+            self.con_perdidas_button.pack(side="top")
+
+            self.salir_button = tk.Button(self.root, text="Salir",bg="red", fg="white", command=self.root.quit)
+            self.salir_button.pack(side="top")
+
 if __name__ == "__main__":
-    g = CalentadorH2O()
-    stop = False
-    while not stop:
-        election = int(input("Ingresar 1 para cálculo sin pérdidas \nIngresar 2 para cálculo con pérdidas\n"))
-        if election == 1:
-            g.sin_perdidas()
-            stop = True
-        elif election == 2:
-            g.con_perdidas()
-            stop = True
-        else:
-            print("Opción no válida, por favor ingrese 1 o 2.")
+    app = CalentadorH2O()
+    app.root.mainloop()
